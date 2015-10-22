20c1b6298d77b6a0a714e81d280a5c25e286bdfb
Andrew Green <afgreen@cern.ch>
2015-10-22 12:57:43 +0100

    Initial commit with working CMakeLists.txt file. No conversion yet.

---
+++ cad2gdml.cc
@@ -0,0 +1,20 @@
+#include "CADMesh.hh"
+
+#include "G4LogicalVolume.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4VSolid.hh"
+#include "G4ThreeVector.hh"
+
+
+
+
+
+int main(int argc, char** argv)
+{
+    if(argc == 1)
+    {
+        G4cout << "Usage:" << G4endl;
+    }
+
+    return 0;
+}
